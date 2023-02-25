e15aebad84f4904b03238ed98e052defe9708314
Alexandre Mutel <alexandre_mutel@live.com>
2023-02-25 08:05:06 +0100

    Try to fix compilation on Windows

--- ext/nplug-proxy/build_nplug_proxy.ps1
+++ ext/nplug-proxy/build_nplug_proxy.ps1
@@ -17,8 +17,13 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandArgumentPassing = "Standard"
 
+$cmake_relative_build_folder = "lib"
+if ($IsWindows) {
+    $cmake_relative_build_folder = "bin"
+}
+
 $cmakeConfig = "Release"
 #$cmakeConfig = "RelWithDebInfo"
 #$cmakeConfig = "Debug"
 
-& "$PSScriptRoot/../CMake-Build-Platforms.ps1" -bit32 $false -BuildFolder "build" -CMakeConfig $cmakeConfig
+& "$PSScriptRoot/../CMake-Build-Platforms.ps1" -bit32 $false -BuildFolder "build" -CMakeConfig $cmakeConfig -CMakeRelativeBuildFolder $cmake_relative_build_folder
